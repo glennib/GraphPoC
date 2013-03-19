793897a43fae474e8870684f240edfd57a149622
kenwi <kenneth@wilhelmsen.nu>
2013-03-19 20:30:48 +0100

    Updated and completed PoC

--- GraphPoC/Form1.Designer.cs
+++ GraphPoC/Form1.Designer.cs
@@ -28,39 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart1);
+            this.splitContainer1.Size = new System.Drawing.Size(932, 586);
+            this.splitContainer1.SplitterDistance = 141;
+            this.splitContainer1.TabIndex = 1;
+            // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Enabled = false;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(621, 547);
-            this.chart1.TabIndex = 0;
+            this.chart1.Size = new System.Drawing.Size(787, 586);
+            this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 547);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(932, 586);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
@@ -68,7 +111,11 @@
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+
     }
 }
 
--- GraphPoC/Form1.cs
+++ GraphPoC/Form1.cs
@@ -9,42 +9,92 @@ using System.Windows.Forms;
 
 namespace GraphPoC
 {
-
-    public class ChartDataPoints
-    {
-        public int ID { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public float Value { get; set; }
-    }
-
     public partial class Form1 : Form
     {
+        // Alle  datapunkt, og nåværende tidspunkt
+        List<ChartDataPoint> allDataPoints = ChartDataPoint.GenerateDataPoints();
+        DateTime currentTime = DateTime.Now;
+
         public Form1()
         {
             InitializeComponent();
+            
+            // Sett opp grafen
+            SetupGraph();
 
+            // Hent ut initielle verdier og kjør databind
+            InitializeGraph( currentTime );
+        }
+
+        public void SetupGraph()
+        {
+            // Sett opp grafen og sett Timestamp og Value som members av klassen ChartDataPoint som skal være de som er henhodsvis akse X og Y
+            chart1.Series.Add("linechart");
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[0].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+            chart1.Series[0].XValueMember = "TimeStamp";
+            chart1.Series[0].YValueMembers = "Value";           
+        }
+
+        public IEnumerable<ChartDataPoint> SelectDataPoints( DateTime date )
+        {
+            // Returner fra allDataPoints de datapunktene som er innenfor tidsrammen som vi ønsker
+            return allDataPoints.Where(c => c.TimeStamp >= date.AddDays(-1) && c.TimeStamp <= date.AddDays(1));
+        }
+
+        public void InitializeGraph( DateTime date )
+        {
+            // Hent ut datapunktene som er ønskelige
+            var selectedDataPoints = SelectDataPoints( date );
+
+            // Legg datapunktene til som DataSource, og kjør databindingen
+            chart1.DataSource = selectedDataPoints; 
+            chart1.DataBind();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Legg til en time, og oppdater graf
+            currentTime = currentTime.AddHours(1);
+            
+            InitializeGraph(currentTime);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Trekk fra en time, og oppdater graf
+            currentTime = currentTime.AddHours(-1);
+
+            InitializeGraph(currentTime);
+        }
+    }
+    
+    // Klasse som beskriver et datapunkt. 
+    public class ChartDataPoint
+    {        
+        public int ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public float Value { get; set; }
+
+        // Genererer opp 100000 datapunkt med tilfeldig verdi og som starter fra for -5 dager fra nå
+        public static List<ChartDataPoint> GenerateDataPoints()
+        {
             Random rand = new Random();
-            DateTime currentTime = DateTime.Today.AddDays( -2 );
-            List<ChartDataPoints> chartDataPoints = new List<ChartDataPoints>();
-            for (int i = 0; i < 100; i++)
+            DateTime currentTime = DateTime.Today.AddDays(-5);
+            List<ChartDataPoint> chartDataPoints = new List<ChartDataPoint>();
+            for (int i = 0; i < 100000; i++)
             {
-                ChartDataPoints dataPoint = new ChartDataPoints()
+                currentTime = currentTime.AddMinutes(25);
+                ChartDataPoint dataPoint = new ChartDataPoint()
                 {
                     ID = i,
-                    TimeStamp = currentTime.AddHours(1),
-                    Value = rand.Next( 1000 )
+                    TimeStamp = currentTime,
+                    Value = rand.Next(1000)
                 };
                 chartDataPoints.Add(dataPoint);
             }
-            
-            chart1.Series.Add("linechart");
-            chart1.DataSource = chartDataPoints;
-            chart1.Series[1].XValueMember = "TimeStamp";
-            chart1.Series[1].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
-            chart1.Series[1].YValueMembers = "Value";
-            chart1.DataBind();
 
-
+            return chartDataPoints;
         }
     }
 }
